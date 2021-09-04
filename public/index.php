84a33752baccfc5fb96c2ff722e1c47ed8889451
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-09-04 10:22:03 +0100

    Update on devoir-app
    create css,js and img directories in public directory

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php 
+use Composer\Autoload\ClassLoader;
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'constants.php';
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'functions.php';
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 echo APPLICATION_NAMESPACE;
