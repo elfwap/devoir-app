fcdd04a5a24d35d362664dbc455f5587576ae428
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-11-18 18:13:41 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,14 @@ require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR 
 use Devoir\Exception\DevoirException;
 use DevoirApp\Application;
 
+if (!isset($ht_accessed)) {
+	if (strpos(PHP_SAPI, 'cli') === 0) {
+		$ht_accessed = false;
+	}
+	elseif (strpos(PHP_SAPI, 'apache') === 0) {
+		$ht_accessed = true;
+	}
+}
 try{
 	$app = new Application(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'system');
 }
