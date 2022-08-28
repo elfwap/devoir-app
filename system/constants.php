d5c35f53030b5ac3d0065e983649310d44d9e387
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2022-08-28 08:18:16 +0100

    Mod

--- system/constants.php
+++ system/constants.php
@@ -1,4 +1,10 @@
 <?php
 
 define('APPLICATION_NAMESPACE', 'DevoirApp\\');
-//define('BASE_PATH', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'src');
+# Uncomment the next 'define' line to set the base path, to the defined path, which can be changed from default.
+# The framework normarly does that automatically.
+//define('BASE_PATH', dirname(dirname(__FILE__)));
+//define('BASE_SOURCE_PATH', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'src');
+define('APP_SYSTEM_PATH', dirname(__FILE__));
+# Same things as 'BASE_SOURCE_PATH'
+define('APP_SOURCE_PATH', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'src');
