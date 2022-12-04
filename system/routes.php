bcf1bc5f19f8f91e9d8bac0abdbc66003941368f
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2022-12-04 15:34:21 +0100

    Fixed routes

--- system/routes.php
+++ system/routes.php
@@ -1,5 +1,55 @@
 <?php
 
 return [
+	'c:=public;a:={8};p:={9}' => [
+		'controller' => 'errors',
+		'action' => 'fours',
+		'params' => '1400/Bad Request'
+	],
+	'c:=resources;a:=images;p:={0}' => [
+		'controller' => 'resources',
+		'action' => 'img',
+		'params' => '$0'
+	],
+	'c:=res;a:=images;p:={10}' => [
+		'controller' => 'resources',
+		'action' => 'img',
+		'params' => '$10'
+	],
+	'c:=res;a:={12};p:={13}' => [
+		'controller' => 'resources',
+		'action' => '$12',
+		'params' => '$13'
+	],
+	'c:=da-content;a:={20};p:={21}' => [
+		'controller' => 'resources',
+		'action' => '$20',
+		'params' => '$21'
+	],
+	'c:=devoir-app-content;a:={22};p:={23}' => [
+		'controller' => 'resources',
+		'action' => '$22',
+		'params' => '$23'
+	],
+	'c:=devoir-app-content;a:=images;p:={24}' => [
+		'controller' => 'resources',
+		'action' => 'img',
+		'params' => '$24'
+	],
+	'c:=css;a:={25};p:={26}' => [
+		'controller' => 'resources',
+		'action' => 'css',
+		'params' => '$25$26'
+	],
+	'c:=js;a:={27};p:={28}' => [
+		'controller' => 'resources',
+		'action' => 'js',
+		'params' => '$27$28'
+	],
+	'c:=img;a:={29};p:={30}' => [
+		'controller' => 'resources',
+		'action' => 'img',
+		'params' => '$29$30'
+	],
 	
 ];
